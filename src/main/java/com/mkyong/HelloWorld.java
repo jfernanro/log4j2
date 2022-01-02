44e3fe4ded6e7c45b7edb6f036ef7367e4642657
jfernanro <juanantoniofernanro@gmail.com>
2022-01-02 19:39:27 +0100

    format

--- src/main/java/com/mkyong/HelloWorld.java
+++ src/main/java/com/mkyong/HelloWorld.java
@@ -3,34 +3,36 @@ package com.mkyong;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class HelloWorld {
+public class HelloWorld
+{
 
   // private static final Logger logger = LogManager.getLogger(HelloWorld.class);
   private static final Logger logger = LoggerFactory.getLogger(HelloWorld.class);
 
-    public static void main(String[] args) {
+  public static void main(String[] args)
+  {
 
-      logger.debug("Hello World from Log4j 2");
+    logger.debug("Hello World from Log4j 2");
 
-        // in old days, we need to check the log level log to increase performance
-        /*if (logger.isDebugEnabled()) {
-            logger.debug("{}", getNumber());
-        }*/
+    // in old days, we need to check the log level log to increase performance
+    /*
+     * if (logger.isDebugEnabled()) { logger.debug("{}", getNumber()); }
+     */
 
-        // with Java 8, we can do this, no need to check the log level
+    // with Java 8, we can do this, no need to check the log level
 
-        for(int i = 0;i <= 10000;i++)
-        {
-          // test rolling file
-          logger.debug("Invocamos a getNumber() : {}",getNumber(i));
-
-        }
+    for(int i = 0;i <= 10000;i++)
+    {
+      // test rolling file
+      logger.debug("Invocamos a getNumber() : {}",getNumber(i));
 
     }
 
-      static int getNumber(int n)
-      {
-        return n * 1;
-    }
+  }
+
+  static int getNumber(int n)
+  {
+    return n * 1;
+  }
 
 }
